35a20176107afe875266faf0d518c348ddcd771f
Mino <minh.lbui@gmail.com>
2025-06-21 13:33:53 +0200

    Angular material and fontawesome

--- src/app/shell/main-layout-with-sidebar/main-layout-with-sidebar.ts
+++ src/app/shell/main-layout-with-sidebar/main-layout-with-sidebar.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { initFlowbite } from 'flowbite';
+import { Sidebar } from "../features/sidebar/sidebar";
+import { MatTabsModule } from '@angular/material/tabs';
 
 @Component({
   selector: 'app-main-layout-with-sidebar',
-  imports: [CommonModule],
+  imports: [CommonModule, Sidebar, MatTabsModule ],
   templateUrl: './main-layout-with-sidebar.html',
   styleUrl: './main-layout-with-sidebar.css',
 })
